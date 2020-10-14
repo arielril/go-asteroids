af99e3e11f896330b9cb83ef190e49a568ba5c27
Ariel Ril <whoami@Whos-Pro.local>
2020-10-14 09:30:03 -0300

    update object package to suport composition for the other packages

--- object/object.go
+++ object/object.go
@@ -4,46 +4,61 @@ import (
 	"github.com/arielril/go-asteroids/point"
 )
 
+// Object is the representation of the master object
 type Object interface {
 	// Draw the object in the window
 	Draw()
 
 	// Get the raw values of the object
-	Raw() *object
+	Raw() *Struct
 
 	RotateRight() Object
 	RotateLeft() Object
 }
 
-type object struct {
-	Data [][]int
+// Data is the formatted data from the template file
+type Data [][]int
+
+// Struct is the representation of an Object
+type Struct struct {
+	Data Data
 
 	Pos point.Point
 
 	Rotation float32
 }
 
-func New() Object {
-	return &object{
-		Pos:      point.New(0, 0, 0),
+// New creates a new object
+func New(data Data, p point.Point) Object {
+	if p == nil {
+		p = point.New(0, 0, 0)
+	}
+
+	return &Struct{
+		Data:     data,
+		Pos:      p,
 		Rotation: 0,
 	}
 }
 
-func (o *object) Raw() *object {
+// Raw gets the raw values from an Object
+func (o *Struct) Raw() *Struct {
 	return o
 }
 
-func (o *object) Draw() {}
+// Draw draws the Object in the OpenGL window
+func (o *Struct) Draw() {}
 
-func _rotate(angle float32, o *object) {
+func _rotate(angle float32, o *Struct) {
 	o.Rotation += angle
 }
 
-func (o *object) RotateRight() Object {
+// RotateRight rotate the object to the right
+func (o *Struct) RotateRight() Object {
 	return o
 }
 
-func (o *object) RotateLeft() Object {
+// RotateLeft rotate the object to the left
+func (o *Struct) RotateLeft() Object {
 	return o
 }
